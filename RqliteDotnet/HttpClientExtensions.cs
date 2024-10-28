a1c205b7528414ff3321a3042dfc5deb62961624
Jonatan Olofsson <jonatan.olofsson@sjofartsverket.se>
2024-10-28 07:55:22 +0100

    Removing nullable return type from HttpClientExtensions.SendTyped

--- RqliteDotnet/HttpClientExtensions.cs
+++ RqliteDotnet/HttpClientExtensions.cs
@@ -4,7 +4,7 @@ namespace RqliteDotnet;
 
 public static class HttpClientExtensions
 {
-    public static async Task<T?> SendTyped<T>(this HttpClient client, HttpRequestMessage request)
+    public static async Task<T> SendTyped<T>(this HttpClient client, HttpRequestMessage request)
     {
         var response = await client.SendAsync(request);
         var content = await response.Content.ReadAsStringAsync();
@@ -13,6 +13,6 @@ public static class HttpClientExtensions
 
         var result = JsonSerializer.Deserialize<T>(content, new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
 
-        return result;
+        return result!;
     }
 }
