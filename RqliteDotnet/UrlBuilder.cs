bb7c49f6d215da019f94a817a730009e20e49ca1
Arthur <arthrp@users.noreply.github.com>
2024-11-06 01:18:31 +0200

    Add read level support for queries and unit tests (#15)

---
+++ RqliteDotnet/UrlBuilder.cs
@@ -0,0 +1,26 @@
+namespace RqliteDotnet;
+
+public static class UrlBuilder
+{
+    public static string Build(string baseUrl, string query, ReadLevel level)
+    {
+        var data = "&q=" + Uri.EscapeDataString(query);
+        var readLevelParam = GetReadLevel(level);
+
+        return $"{baseUrl}{data}{readLevelParam}";
+    }
+    
+    private static string GetReadLevel(ReadLevel level)
+    {
+        var result = level switch
+        {
+            ReadLevel.Default => "",
+            ReadLevel.Weak => "&level=weak",
+            ReadLevel.Linearizable => "&level=linearizable",
+            ReadLevel.Strong => "&level=strong",
+            ReadLevel.None => "&level=none",
+            ReadLevel.Auto => "&level=auto"
+        };
+        return result;
+    }
+}
