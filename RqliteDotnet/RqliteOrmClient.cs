14d3157a5accb54fd000005f3f6adf259bfcf572
Arthur <arthrp@users.noreply.github.com>
2025-11-13 00:33:07 +0200

    Get rid of warnings (#49)

--- RqliteDotnet/RqliteOrmClient.cs
+++ RqliteDotnet/RqliteOrmClient.cs
@@ -11,24 +11,27 @@ public class RqliteOrmClient : RqliteClient, IRqliteOrmClient
     public async Task<List<T>> Query<T>(string query) where T : new()
     {
         var response = await Query(query);
-        if (response.Results!.Count > 1)
+        if (response?.Results!.Count > 1)
             throw new DataException("Query returned more than 1 result. At the moment only 1 result supported");
-        var res = response.Results[0];
+        var res = response?.Results?[0];
 
-        if (!string.IsNullOrEmpty(res.Error))
+        if (!string.IsNullOrEmpty(res?.Error))
             throw new InvalidOperationException(res.Error);
         var list = new List<T>();
 
-        for (int i = 0; i < res.Values?.Count; i++)
+        for (int i = 0; i < res?.Values?.Count; i++)
         {
             var dto = new T();
 
             foreach (var prop in typeof(T).GetProperties())
             {
-                var index = res.Columns.FindIndex(c => c.ToLower() == prop.Name.ToLower());
-                var val = GetValue(res.Types[index], res.Values[i][index]);
+                if (res.Columns != null)
+                {
+                    var index = res.Columns.FindIndex(c => c.ToLower() == prop.Name.ToLower());
+                    var val = GetValue(res.Types?[index], res.Values[i][index]);
 
-                prop.SetValue(dto, val);
+                    prop.SetValue(dto, val);
+                }
             }
 
             list.Add(dto);
@@ -56,10 +59,13 @@ public class RqliteOrmClient : RqliteClient, IRqliteOrmClient
 
             foreach (var prop in typeof(U).GetProperties())
             {
-                var index = res.Columns.FindIndex(c => c.ToLower() == prop.Name.ToLower());
-                var val = GetValue(res.Types[index], res.Values[i][index]);
+                if (res.Columns != null)
+                {
+                    var index = res.Columns.FindIndex(c => c.ToLower() == prop.Name.ToLower());
+                    var val = GetValue(res.Types?[index], res.Values[i][index]);
 
-                prop.SetValue(dto, val);
+                    prop.SetValue(dto, val);
+                }
             }
 
             list.Add(dto);
