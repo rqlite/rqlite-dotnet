8ba35094b2c32980405302be6fa6a696e8b6b1e9
Arthur <arthrp@users.noreply.github.com>
2026-02-12 22:40:04 +0200

    Enforce formatting

--- RqliteDotnetExample/Program.cs
+++ RqliteDotnetExample/Program.cs
@@ -4,6 +4,7 @@ namespace RqliteDotnetExample;
 
 public static class RqliteDotnetExample
 {
+#pragma warning disable IDE0060
     public static async Task Main(string[] args)
     {
         var x = new RqliteClient("http://localhost:4001");
